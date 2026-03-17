c16578779826ebfcae4f6ccc5096eee5025eea34
Phuc270205 <votahoangphuc0@gmail.com>
2026-03-17 14:03:45 +1100

    Update

--- Data Cleaning project/NashVille_Housing.sql
+++ Data Cleaning project/NashVille_Housing.sql
@@ -26,7 +26,7 @@ Select *
 From SQL_Project.dbo.NashvilleHousing
 order by ParcelID
 
-
+--
 
 Select a.ParcelID, a.PropertyAddress, b.ParcelID, b.PropertyAddress, ISNULL(a.PropertyAddress,  b.PropertyAddress)
 From SQL_Project.dbo.NashvilleHousing a
@@ -34,3 +34,25 @@ JOIN SQL_Project.dbo.NashvilleHousing b
 	on a.ParcelID = b.ParcelID
 	AND a.[UniqueID ] <> b.[UniqueID]
 Where a.PropertyAddress is null
+
+
+Update a
+SET PropertyAddress = ISNULL(a.PropertyAddress,  b.PropertyAddress)
+From SQL_Project.dbo.NashvilleHousing a
+JOIN SQL_Project.dbo.NashvilleHousing b
+	on a.ParcelID = b.ParcelID
+	AND a.[UniqueID ] <> b.[UniqueID]
+Where a.PropertyAddress is null
+
+--------------------------------
+
+-- Seperate the Adress into individual columns (Adrress, City, State)
+
+Select PropertyAddress
+From SQL_Project.dbo.NashvilleHousing
+
+SELECT
+SUBSTRING(PropertyAddress, 1, CHARINDEX(',', PropertyAddress) -1 ) as Address,
+SUBSTRING(PropertyAddress, CHARINDEX(',', PropertyAddress) +1, LEN(PropertyAddress)) as Address
+
+From SQL_Project.dbo.NashvilleHousing
